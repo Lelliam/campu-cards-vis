102a6a8fef4b8b5fc84aa52685fad616e78fead9
whale-52hz <liuliangswust@qq.com>
2019-12-23 15:36:02 +0800

    老唐Start

---
+++ client/src/components/Function05/AppRadar.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="radar"></div>
+</template>
+
+<script>
+    export default {
+        name: "AppRadar"
+    }
+</script>
+
+<style scoped>
+  #radar{
+    position: absolute;
+    bottom:0;
+    right: 0;
+    width: 25%;
+    height: 30%;
+    background-color: #ff7c8b;
+  }
+</style>
---
+++ client/src/components/Function05/AppTimeLine.vue
@@ -0,0 +1,20 @@
+<template>
+<div id="time_line"></div>
+</template>
+
+<script>
+    export default {
+        name: "AppTimeLine"
+    }
+</script>
+
+<style scoped>
+  #time_line{
+    position: absolute;
+    bottom:0;
+    left: 25%;
+    width: 50%;
+    height: 30%;
+    background-color: #ffb553;
+  }
+</style>
